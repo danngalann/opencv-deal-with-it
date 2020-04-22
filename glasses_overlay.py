b1e88b5041ffc5f582de49130487a72df1661ec0
danngalann <47415745+danngalann@users.noreply.github.com>
2020-04-22 08:41:31 +0200

    Added function to overlay image

--- glasses_overlay.py
+++ glasses_overlay.py
@@ -1,6 +1,33 @@
 from imutils.video import WebcamVideoStream
+from imutils import face_utils
 import imutils
 import cv2
+import numpy as np
+import json
+import dlib
+
+
+# Overlays an image
+def overlay_image(bg, fg, fgMask, coords):
+    (sH, sW) = fg.shape[:2]
+    (x, y) = coords
+    # the overlay should be the same width and height as the input
+    # image and be totally blank *except* for the foreground which
+    # we add to the overlay via array slicing
+    overlay = np.zeros(bg.shape, dtype="uint8")
+    overlay[y:y + sH, x:x + sW] = fg
+    # the alpha channel, which controls *where* and *how much*
+    # transparency a given region has, should also be the same
+    # width and height as the input image, but will contain only
+    # the foreground mask
+    alpha = np.zeros(bg.shape[:2], dtype="uint8")
+    alpha[y:y + sH, x:x + sW] = fgMask
+    alpha = np.dstack([alpha] * 3)
+    # perform alpha blending to merge the foreground, background,
+    # and alpha channel together
+    output = alpha_blend(overlay, bg, alpha)
+
+    return output
 
 vs = WebcamVideoStream(src=0).start()
 
